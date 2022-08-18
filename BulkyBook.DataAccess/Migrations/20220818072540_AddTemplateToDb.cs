18e4982924b8b951c2b033292f03ee3c87ebe475
Paresh Sethy <psethy@argusoft.com>
2022-08-18 15:39:58 +0530

    Added file to local repo

---
+++ BulkyBook.DataAccess/Migrations/20220818072540_AddTemplateToDb.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BulkyBook.DataAccess.Migrations
+{
+    public partial class AddTemplateToDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Templates",
+                columns: table => new
+                {
+                    TemplateId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TemplateName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Order = table.Column<int>(type: "int", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CreatedBy = table.Column<int>(type: "int", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedBy = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Templates", x => x.TemplateId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Templates");
+        }
+    }
+}
---
+++ BulkyBookWeb/Controllers/TemplateController.cs
@@ -0,0 +1,40 @@
+﻿using BulkyBook.DataAccess;
+using BulkyBook.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace BulkyBookWeb.Controllers
+{
+    public class TemplateController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public TemplateController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            var objectTemplateList = _db.Templates.ToList();
+            return View(objectTemplateList);
+        }
+
+        //GET 
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+
+        //Post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Template obj)
+        {
+            _db.Templates.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
